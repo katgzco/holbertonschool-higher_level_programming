ca0598ca58297097eb7bd944ac304c3c39c2ed01
katgzco <katgzco@gmail.com>
2020-12-08 19:15:43 -0500

    Fix, add text in last

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+lastdigit = number % 10
 if (number < 0):
     lastdigit = number % -10
-else:
-    lastdigit = number % 10
 print("Last digit of {} is ".format(number), end="")
 if (lastdigit > 5):
     print("{} and is greater than 5".format(lastdigit))
 elif (lastdigit == 0):
     print("{} and is 0".format(lastdigit))
 else:
-    print("is {} and is less than 6 and not 0".format(lastdigit))
+    print("{} and is less than 6 and not 0".format(lastdigit))
