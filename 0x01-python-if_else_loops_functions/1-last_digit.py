b3d4d3383db1f011033af431674aea7fcbc58f85
katgzco <katgzco@gmail.com>
2020-12-08 19:11:50 -0500

    Change if

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# Use the constructors to do the convertions.
-lastdigit = int(str(number)[-1])
+lastdigit = number % 10
 if (number < 0):
-    lastdigit *= -1
+    lastdigit = number % -10
+print("Last digit of {} is ".format(number), end="")
 if (lastdigit > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, lastdigit))
+    print("{} and is greater than 5".format(lastdigit))
 elif (lastdigit == 0):
-    print("Last digit of {} is {} and is 0".format(number, lastdigit))
+    print("{} and is 0".format(lastdigit))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastdigit))
+    print("is {} and is less than 6 and not 0".format(lastdigit))
