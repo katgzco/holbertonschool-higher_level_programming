a54847c20557c9cba33661184ce5ff6b7fbd7341
katgzco <katgzco@gmail.com>
2020-12-10 19:36:44 -0500

    Fix if

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     list_names = dir(hidden_4)
     for i in list_names:
-        if list_names[1] != "_":
-            print(list_names)
+        if i[1] != "_":
+            print(i)
