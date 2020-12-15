4a362b9c705644521d9760eabe14591d178a3acd
katgzco <katgzco@gmail.com>
2020-12-14 22:06:43 -0500

    113 task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *h_ref_first = *head, *h_ref_second = (*head)->next;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+
+/*check if is list is empty or the first node*/
+	if (*head == NULL || h_ref_first->next == NULL)
+	{
+		new_node->next = *head;
+		*head = new_node->next;
+	}
+
+/*check if is in the middle*/
+	while (h_ref_first->next != NULL)
+	{
+		if (h_ref_first->n < number && h_ref_second->n > number)
+		{
+			h_ref_first->next = new_node;
+			new_node = h_ref_second;
+		}
+	h_ref_first = h_ref_first->next;
+	h_ref_second = h_ref_second->next;
+	}
+/*is the last*/
+	h_ref_first->next = new_node;
+	new_node->next = NULL;
+	return (new_node);
+}
