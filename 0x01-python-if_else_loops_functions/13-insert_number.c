92663fafd75bcdba1153927d76724f83201bbf58
katgzco <katgzco@gmail.com>
2020-12-14 22:46:18 -0500

    Fix logic

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,19 +12,23 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 
 /*check if is list is empty or the first node*/
-	if (*head == NULL || h_ref_first->next == NULL)
+	if (h_ref_first->next != NULL && number < (h_ref_first->n))
 	{
+		printf("entrer al ptrimero  h_ref %d  nu,ber %d\n", h_ref_first->n, number);
+		new_node->n = number;
 		new_node->next = *head;
-		*head = new_node->next;
+		*head = new_node;
+		return (new_node);
 	}
 
 /*check if is in the middle*/
-	while (h_ref_first->next != NULL)
+	while (h_ref_second != NULL)
 	{
 		if (h_ref_first->n < number && h_ref_second->n > number)
 		{
+			new_node->next = h_ref_first->next;
 			h_ref_first->next = new_node;
-			new_node = h_ref_second;
+			return (new_node);
 		}
 	h_ref_first = h_ref_first->next;
 	h_ref_second = h_ref_second->next;
