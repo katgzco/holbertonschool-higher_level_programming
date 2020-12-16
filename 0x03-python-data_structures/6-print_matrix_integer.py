2a3abff904f0c6e03ef4cc0f230f2a344eb748d1
katgzco <katgzco@gmail.com>
2020-12-15 22:23:02 -0500

    Add format in print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         spac = ""
         for column in row:
-            print("{}{}".format(spac, column), end="")
+            print("{:s}{:d}".format(spac, column), end="")
             spac = " "
         print()
