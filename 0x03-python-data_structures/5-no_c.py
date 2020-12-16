c1ab2b0d49d46182528cb2d9e0a7608c474046cd
katgzco <katgzco@gmail.com>
2020-12-15 22:05:43 -0500

    First  upload

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for index in range(len(my_string)):
+        if my_string[index] == "c" or my_string[index] == "C":
+            continue
+        new_string = new_string + my_string[index]
+    return(new_string)
