5cd286d8e8c26135a93e27b81ec485606a1106b1
katgzco <katgzco@gmail.com>
2020-12-15 23:49:22 -0500

    Change for every value of the list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,29 +8,36 @@
 int is_palindrome(listint_t **head)
 {
 	int list_length = 0, reverse_num = 0, tmp_num = 0;
+	listint_t *h_reference = *head;
 
 	if (head != NULL)
 	{
 		if (*head == NULL)
 			return (1);
 
-		list_length = (*head)->n;
-		/*reverse nummber*/
-		tmp_num = list_length;
-		if (list_length > 9)
+		while (h_reference->next != NULL)
 		{
-			while (tmp_num)
+
+			list_length = h_reference->n;
+			/*reverse nummber*/
+			tmp_num = list_length;
+			if (list_length > 9)
 			{
-				reverse_num = 10 * reverse_num + (tmp_num % 10);
-				tmp_num /= 10;
+				while (tmp_num)
+				{
+					reverse_num = 10 * reverse_num + (tmp_num % 10);
+					tmp_num /= 10;
+				}
 			}
-		}
-		else
-			return (1);
+			else
+				return (1);
 
 		/*check if is polindrome*/
-		if (list_length == reverse_num)
-			return (1);
+			if (list_length == reverse_num)
+				return (1);
+
+			h_reference = h_reference->next;
+		}
 	}
 	return (0);
 }
