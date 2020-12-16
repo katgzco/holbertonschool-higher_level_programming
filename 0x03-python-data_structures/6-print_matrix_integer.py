1236aa222466bf0f937ded4bbecf1daffa564067
katgzco <katgzco@gmail.com>
2020-12-15 22:20:27 -0500

    Fix space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        spac = ""
         for column in row:
-            print("{}".format(column), end=" ")
+            print("{}{}".format(spac, column), end="")
+            spac = " "
         print()
