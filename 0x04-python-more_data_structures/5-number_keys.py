41a1ecaa1074324e4f37d76fc83f8170c9378519
katgzco <katgzco@gmail.com>
2020-12-18 11:56:15 -0500

    fix edge case

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for num, keys in enumerate(a_dictionary):
-        pass
-    return num + 1
+    if a_dictionary:
+        for num, keys in enumerate(a_dictionary):
+            pass
+        return num + 1
