0409cd1ce988010488a7be46b5f6b18a813bb814
katgzco <katgzco@gmail.com>
2020-12-18 12:15:01 -0500

    Change logic

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    if a_dictionary:
-        for num, keys in enumerate(a_dictionary):
-            pass
-        return num + 1
+        return len(a_dictionary)
