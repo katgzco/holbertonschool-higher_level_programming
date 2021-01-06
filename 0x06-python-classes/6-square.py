e07232c03b83422ec7e8565760f69ee3a6e0ce5f
katgzco <katgzco@gmail.com>
2021-01-05 23:13:50 -0500

    Fix - 3 vrsion - fix the spaces with position in 1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,10 +50,11 @@ class Square:
 
     def my_print(self):
 
-        if self.__size == 0 and self.__position[1] == 0:
+        if self.__size == 0:
             print()
-        elif self.__position[1] != 0:
-            print()
+        if self.__position[1] != 0:
+            for j in range(self.__position[1]):
+                print()
 
         for i in range(self.__size):
             if self.__position[0] != 0:
