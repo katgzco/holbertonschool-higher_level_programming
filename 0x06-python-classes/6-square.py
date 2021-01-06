75dd17fb7884b3bdbde212889db0825bdc142b7e
katgzco <katgzco@gmail.com>
2021-01-05 23:50:46 -0500

    Fix 5- fix else in print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,21 +17,6 @@ class Square:
     def area(self):
         return self.__size**2
 
-    """ prints in stdout the square with the character #"""
-
-    def my_print(self):
-
-        if self.__size == 0:
-            print()
-
-        for row in range(self.__position[1]):
-            print()
-
-        for i in range(self.__size):
-            if self.__position[0] != 0:
-                print(" " * self.__position[0], end="")
-            print("#" * self.__size)
-
     """Getter"""
     @property
     def size(self):
@@ -51,6 +36,21 @@ class Square:
     def position(self):
         return self.position
 
+    """ prints in stdout the square with the character #"""
+
+    def my_print(self):
+
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__position[1]):
+                print()
+
+            for i in range(self.__size):
+                if self.__position[0] != 0:
+                    print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+
     """Setter"""
     @position.setter
     def position(self, value):
@@ -58,5 +58,4 @@ class Square:
                 value[0] < 0 or value[1] < 0 or \
                 type(value[1]) is not int or type(value[0]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
