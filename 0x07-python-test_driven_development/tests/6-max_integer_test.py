928c58e8fd66431b38eeb8c9630a87e0911b1a72
katgzco <katgzco@gmail.com>
2021-01-09 12:51:55 -0500

    Fix sintax ion test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,16 +14,18 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_integer(self):
+        "Test numbers cases"
         self.assertEqual(max_integer([2, 4, 5, 6, 7]), 7)
         self.assertEqual(max_integer([2, 4, 200, 6, 7]), 200)
         self.assertEqual(max_integer([-2, -4, -5, -100, -7]), 100)
         self.assertEqual(max_integer([2.3, 4.6, 5.3, 6, 323]), 323)
-        self.assertEqual(max_integer([45]), 54)
+        self.assertEqual(max_integer([45]), 45)
 
     def test_void(self):
+        "Test empty"
         self.assertEqual(max_integer([]), None)
 
     def test_raise(self):
+        "Test error cases"
         self.assertRaises(TypeError, max_integer, None)
-        self.assertRaises(TypeError, max_integer, 5)
         self.assertRaises(TypeError, max_integer, {2, 8, 9})
