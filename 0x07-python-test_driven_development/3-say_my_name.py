f9b035e5a72db051df1f0e9fdcf26b2f9554b8eb
katgzco <katgzco@gmail.com>
2021-01-09 15:50:13 -0500

    Revers change

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ This is the module for
+    say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        say_my_name
+    """
+    first = isinstance(first_name, str)
+    last = isinstance(last_name, str)
+    if first is False or last is False:
+        raise TypeError("{} must be a string".format
+                        ("first_name"if first is False else "last_name"))
+    print("My name is {} {}".format(first_name, last_name))
