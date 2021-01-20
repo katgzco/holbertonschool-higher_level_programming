d031b2d859fc3fc9fdd2442fc71a0612e444e24e
katgzco <katgzco@gmail.com>
2021-01-19 23:13:17 -0500

    Change format

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,30 +5,20 @@ Class BaseGeometry
 
 
 class BaseGeometry():
+    """"
+    Template for a base geometry object
     """
-    Template for a base geometry
-    """
-
     def __init__(self):
         """ Initial constructor space """
         pass
 
     def area(self):
-        """
-        raises an Exception
-        """
-        if self:
-            raise Exception("area() is not implemented")
+        """ Space to calculate the area (in progress) """
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        validates if value is integer
-        Parameters
-        ----------
-        name : is a variable for store the value
-        value : is the value to validate
-    """
+        """validates an integer number """
         if (type(value) is not int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise TypeError(f"{name} must be greater than 0")
+            raise TypeError(str(name) + " must be an integer")
+        if (value <= 0):
+            raise ValueError(str(name) + " must be greater than 0")
