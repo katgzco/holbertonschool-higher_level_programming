5ff2b10e319cdde743cf59dcffb1fe9ef2117b0f
katgzco <katgzco@gmail.com>
2021-01-20 23:37:47 -0500

    Fix - change rewrite in the file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""this file receives the standard input"""
+""" module that take argv """
 import sys
-import os.path
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-if __name__ == '__main__':
-    """reads file if possible and append new elements"""
-    filename = "add_item.json"
-    adding_list = sys.argv[1:]
-    old_list = []
-    # load object
-    if (os.path.exists(filename)):
-        old_list = load_from_json_file(filename)
-    new_list = old_list + (adding_list)
-    save_to_json_file(new_list, filename)
+try:
+    file = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    file = []
+for x in sys.argv[1:]:
+    file.append(x)
+save_to_json_file(file, "add_item.json")
