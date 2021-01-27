b0d0df22df76f80aab60fe22c5c16f05c0eab2e2
katgzco <katgzco@gmail.com>
2021-01-27 17:09:59 -0500

    change test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,47 +1,18 @@
 #!/usr/bin/python3
-""" test module Base"""
+""" Test's for base class """
 
 from unittest import TestCase
 from models.base import Base
-import json
-import inspect
 
 
 class TestBase(TestCase):
-    """
-    Class to check base
-    """
-    def test_A_id(self):
+    """ Test's for base class methods """
+
+    def test___init__(self):
+        """ Test's for init method """
+
+        self.assertTrue(Base.__init__.__doc__)
         self.assertEqual(Base().id, 1)
-        self.assertEqual(Base().id, 2)
         self.assertEqual(Base(12).id, 12)
         self.assertEqual(Base("2").id, "2")
-
-    def test_B_to_json_string(self):
-        """
-        test to_json_string
-        """
-        self.assertEqual(Base.to_json_string(None), [])
-        self.assertEqual(Base.to_json_string([]), [])
-        self.assertEqual(Base.to_json_string([{'id': 12}]),
-                         json.dumps([{'id': 12}]))
-        self.assertEqual(Base.to_json_string([{'id': 12}]),
-                         json.dumps([{'id': 12}]))
-
-    def test_C_from_json_string(self):
-        """
-        test from_json_string
-        """
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string("[]"), [])
-        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{"id": 89}])
-
-    def test_D_documentation(self):
-        """
-        test documentation
-        """
-        self.assertTrue(len(Base.__doc__) > 0)
-        methods = inspect.getmembers(Base, predictive=inspect.inmethod)
-
-        for name, method in methods:
-            self.assertTrue(len(methods.__doc__) > 0)
+        self.assertEqual(Base().id, 2)
