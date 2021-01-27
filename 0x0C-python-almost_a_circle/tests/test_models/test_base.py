2d047574d7f153c120501be27a454f2482749d3c
katgzco <katgzco@gmail.com>
2021-01-27 17:28:37 -0500

    change way to test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,7 @@
 
 from unittest import TestCase
 from models.base import Base
+from models import base
 
 
 class TestBase(TestCase):
@@ -16,3 +17,16 @@ class TestBase(TestCase):
         self.assertEqual(Base(12).id, 12)
         self.assertEqual(Base("2").id, "2")
         self.assertEqual(Base().id, 2)
+
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(base.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(Base):
+            self.assertTrue(len(func.__doc__) > 0)
