3f56e724f0af9d6f9e0051161e2e6473dbf6bdea
katgzco <katgzco@gmail.com>
2021-08-02 22:52:05 -0500

    Fix - remove ; of shebang:

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
-#!/usr/bin/node;
+#!/usr/bin/node
 function factorial (number) {
-  if (isNaN(number) || number < 2) {
+  if (isNaN(number) || number === 1 || number === 0) {
     return 1;
   } else {
     return number * factorial(number - 1);
