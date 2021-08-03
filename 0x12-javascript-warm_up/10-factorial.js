60b4ece5d043f849c0e969e49de3e39b026e3427
katgzco <katgzco@gmail.com>
2021-08-02 22:47:21 -0500

    Style - fix idenation in function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (number) {
   if (isNaN(number) || number === 1 || number === 0) {
     return 1;
   } else {
-    return (number * factorial(number - 1));
+    return number * factorial(number - 1);
   }
 }
 
