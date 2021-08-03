a1b16eba7b77c4d0815b970eeb03e4679d0ba58f
katgzco <katgzco@gmail.com>
2021-08-02 22:06:37 -0500

    Refactor - remove variable for print in console log

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-let argumentStatus;
 if (process.argv.length === 2) {
-  argumentStatus = 'No argument';
-} else if (process.argv.length === 3){
-  argumentStatus = 'Argument found';
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  argumentStatus = 'Arguments found';
+ console.log('Arguments found');
 }
-console.log(argumentStatus);
