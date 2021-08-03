09e119df5a92eaab99804c214392827ba0a916e5
katgzco <katgzco@gmail.com>
2021-08-02 22:56:27 -0500

    Fix - remove import statment of argv

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-import { argv } from 'process';
 const arrayForSort = [];
 let i;
-if (argv.length <= 3) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (i in argv) {
-    arrayForSort.push(parseInt(argv[i]));
+  for (i in process.argv) {
+    arrayForSort.push(parseInt(process.argv[i]));
   }
   arrayForSort.sort(function (a, b) { return a - b; });
   console.log(arrayForSort[i - 1]);
