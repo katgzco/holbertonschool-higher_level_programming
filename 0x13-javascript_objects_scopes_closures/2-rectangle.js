8d39a9d3c6db77f9433827d77989e25d3b2968d6
katgzco <katgzco@gmail.com>
2021-08-03 17:43:05 -0500

    Style - correct error in semistandar style

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 }
