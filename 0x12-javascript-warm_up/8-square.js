afd4a46594146b45684a4f99b2f52caad19d94e1
katgzco <katgzco@gmail.com>
2021-08-02 22:34:14 -0500

    Tets - add evaluation to the condition

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const FIRST_ARGUMENT = process.argv[2];
 const PARSE_INT_RESULT = parseInt(FIRST_ARGUMENT);
 let column = 0;
 
-if (isNaN(PARSE_INT_RESULT) && PARSE_INT_RESULT > 0) {
+if (isNaN(PARSE_INT_RESULT) && PARSE_INT_RESULT  === 0) {
   console.log('Missing size');
 } else {
   while (column < PARSE_INT_RESULT) {
