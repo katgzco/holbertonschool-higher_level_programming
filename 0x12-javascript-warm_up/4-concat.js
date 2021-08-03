c3c122c178e066bf1c5b7b1f9e9e0dadd722f7d5
katgzco <katgzco@gmail.com>
2021-08-02 22:24:44 -0500

    Fix - remove import statment of argv

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-import { argv } from 'process';
-const FIRST_ARGUMENT = argv[2];
-const SECOND_ARGUMENT = argv[3];
-
+const FIRST_ARGUMENT = process.argv[2];
+const SECOND_ARGUMENT = process.argv[3];
 console.log(`${FIRST_ARGUMENT} is ${SECOND_ARGUMENT}`);
