e127137e4064fbff4caa626c3c33931850da9583
katgzco <katgzco@gmail.com>
2021-08-02 22:43:06 -0500

    Fix - remove import statment of argv

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
-#!/usr/bin/node
-import { argv } from 'process';
+#!/usr/bin/node;
 function factorial (number) {
   if (isNaN(number) || number === 0 || number === 1) { return 1; } else { return (number * factorial(number - 1)); }
 }
 
-console.log(factorial(argv[2]));
+console.log(factorial(process.argv[2]));
