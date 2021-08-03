d6190ffa14bbdecd1f32e478e798326efea13036
katgzco <katgzco@gmail.com>
2021-08-02 22:01:14 -0500

    Fix - removing the import statment of argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-import { argv } from 'process';
-
 let argumentStatus;
-
-if (argv.length === 2) {
+if (process.argv.length === 2) {
   argumentStatus = 'No argument';
-} else if (argv.length === 3){
+} else if (process.argv.length === 3){
   argumentStatus = 'Argument found';
 } else{
   argumentStatus = 'Arguments found';
