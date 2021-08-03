5d9a8502142744c51d1e8b9e893c7e9cc96247ee
katgzco <katgzco@gmail.com>
2021-08-03 17:41:57 -0500

    Fix - remove inizalization from condition in contructor

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      w = undefined;
-      h = undefined;
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
