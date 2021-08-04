d20ef39f0f6295d673e9516ce419114687e98337
katgzco <katgzco@gmail.com>
2021-08-03 21:20:26 -0500

    Feature - add instance method to the Square Class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c && typeof c === 'string' && c.length === 1) {
+      let column = this.height;
+      while (column > 0) {
+        console.log('C'.repeat(this.width));
+        column--;
+      }
+    } else {
+      this.print();
+    }
+  }
+};
