4445d620cbfcc445ea30aa715b3c7a70d1a82ca6
katgzco <katgzco@gmail.com>
2021-08-03 21:49:40 -0500

    Fix: change the validation in the if of charPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const SquareParent = require('./5-square');
 
 class Square extends SquareParent {
   charPrint (c) {
-    if (c && c === 'string') {
+    if (c && typeof c === 'string') {
       let column = this.height;
       while (column > 0) {
         console.log(c.repeat(this.width));
