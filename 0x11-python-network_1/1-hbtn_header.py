a133820b0c85ae1673ff2a7c8e53ed166cccec87
katgzco <katgzco@gmail.com>
2021-08-24 22:11:28 -0500

    Style: put library outside condition

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable found in the 
-    header of the response
-    """
-if __name__ == "__main__":
+"""displays the value of the X-Request-Id variable found in the"""
 
-    from urllib import request
-    from sys import argv
+from urllib import request
+from sys import argv
 
-    req = argv[1]
+if __name__ == "__main__"
+req = argv[1]
 
-    with request.urlopen(req) as response:
-        print(response.headers['X-Request-Id'])
+with request.urlopen(req) as response:
+    print(response.headers['X-Request-Id'])
