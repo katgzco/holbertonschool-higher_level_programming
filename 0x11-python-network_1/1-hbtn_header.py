dafbd91b0b3adeac1d25c53ce5f795e29ae173c4
katgzco <katgzco@gmail.com>
2021-08-24 22:14:51 -0500

    Fix: use get in header acces

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in the"""
+"""displays the value of the X-Request-Id variable"""
 
 from urllib import request
 from sys import argv
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     req = argv[1]
 
     with request.urlopen(req) as response:
-        print(response.headers['X-Request-Id'])
+        print(response.headers.get('X-Request-Id'))
