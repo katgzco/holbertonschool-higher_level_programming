6f29f289acd93aca2e9717f958556a1b3da8b276
katgzco <katgzco@gmail.com>
2021-08-24 23:29:04 -0500

    Feature: add 7 task

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+ sends a request to the URL and displays the body of the response.
+"""
+
+import sys
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print('Error code: {}'.format(response.status_code))
+    else:
+        print(response.text)
