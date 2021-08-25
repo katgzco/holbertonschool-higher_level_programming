afa58db8bad05be30cbecd6a02d05e5c92b58979
katgzco <katgzco@gmail.com>
2021-08-24 23:16:36 -0500

    Style: add space  between shebang and documentation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 """ Python script that takes in a URL and an email, sends a 
-POST request to the passed URL
-with the email as a parameter, and displays the body of the response"""
+    POST request to the passed URL
+    with the email as a parameter, and displays the body of the response"""
 
 import urllib.request
 import urllib.parse
