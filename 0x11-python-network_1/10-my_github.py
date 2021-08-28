f5fc9fecdb66da295e75be22712aa9aebaa330aa
katgzco <katgzco@gmail.com>
2021-08-28 18:36:00 -0500

    ix: add underscrore in condition

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-''' Python script that takes your GitHub
+'''Python script that takes your GitHub
 credentials (username and password) and uses the
 GitHub API to display your id'''
 
@@ -8,7 +8,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import sys
 
-if name == "main":
+if __name__ == "__main__":
     response = requests.get('https://api.github.com/user',
                             auth=HTTPBasicAuth(sys.argv[1],
                                                sys.argv[2]))
