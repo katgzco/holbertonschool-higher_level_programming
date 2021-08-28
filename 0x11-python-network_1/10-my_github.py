fe1067fca4511e6d6fbc831e5ca223e9cbdc9fa9
katgzco <katgzco@gmail.com>
2021-08-28 18:31:40 -0500

    Featrue: add task 19

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+''' Python script that takes your GitHub
+credentials (username and password) and uses the
+GitHub API to display your id'''
+
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if name == "main":
+    response = requests.get('https://api.github.com/user',
+                            auth=HTTPBasicAuth(sys.argv[1],
+                                               sys.argv[2]))
+    print(response.json().get('id'))
