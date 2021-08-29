4a2f7fe6089cced19e521ff1cbcee3c86337dd4b
katgzco <katgzco@gmail.com>
2021-08-28 19:35:36 -0500

    Fix: format encode

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,8 +4,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https: // api.github.com/repos/%7B % 7D/%7B % 7D/commits % 27/
-    .format(sys.argv[2], sys.argv[1])
+    url = 'https: // api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[2], sys.argv[1])
     r = requests.get(url)
     data = r.json()
     for i in range(len(data)):
