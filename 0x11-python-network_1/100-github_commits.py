7d55d4d3d121b512b30fbeca05c7558a131f38c6
katgzco <katgzco@gmail.com>
2021-08-28 19:32:00 -0500

    Feature: add advance task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''evaluates candidates applying for a back-end position'''
+import requests
+import sys
+
+if name == "main":
+    url = 'https: // api.github.com/repos/%7B % 7D/%7B % 7D/commits % 27/
+    .format(sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    data = r.json()
+    for i in range(len(data)):
+        if i < 10:
+            print('{}: {}'.format(data[i]['sha'],
+                  data[i]['commit']['author']['name']))
+        else:
+            break
